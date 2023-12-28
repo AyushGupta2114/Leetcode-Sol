85e2487ea8d0d18f4e7ad3b64ea6ae331cc3038d
Ayush Gupta <94870982+AyushGupta2114@users.noreply.github.com>
2023-12-29 04:06:00 +0530

    Create file

---
+++ 19-RemoveNthNodeFromEndofList/19-RemoveNthNodeFromEndofList.cpp
@@ -0,0 +1,11 @@
+        }
+            fast = fast->next;
+        for(int i = 0; i < n; ++i) {
+        start->next = head;
+        ListNode* slow = start;
+        ListNode* fast = start;
+        ListNode* start = new ListNode();
+    ListNode* removeNthFromEnd(ListNode* head, int n) {
+class Solution {
+public:
+[1,2,3,4,5]
