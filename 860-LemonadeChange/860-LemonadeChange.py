c1f178266ba110cf5e01a4d7e326fa603d588faf
Ayush Gupta <94870982+AyushGupta2114@users.noreply.github.com>
2023-12-30 03:00:34 +0530

    Update file 12/30/2023, 3:00:34 AM

--- 860-LemonadeChange/860-LemonadeChange.py
+++ 860-LemonadeChange/860-LemonadeChange.py
@@ -1,18 +1,17 @@
-                    continue
                 if num_of_5 >= 1:
                     num_of_5 -= 1
-                shopper += i - 5
+                shopper += i 
                 num_of_10 += 1
             elif i == 10:
                 shopper += i
                 num_of_5 += 1
             if i == 5:
         for i in bills:
-                else:
-                    return False
-            else:
-                shopper += 5
-                shopper += 20
-                if num_of_10 >= 1 and num_of_5 >= 1:
-                    num_of_10 -= 1
-[5,5,5,10,20]
+                shopper -= 5
+        shopper = 0
+        # num_of_20 = 0
+        num_of_5 = 0
+        num_of_10 = 0
+    def lemonadeChange(self, bills: List[int]) -> bool:
+class Solution:
+[
