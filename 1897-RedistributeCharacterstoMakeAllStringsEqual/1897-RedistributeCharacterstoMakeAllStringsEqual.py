f82cb956dd3e7b4bde2f80ebbf957a081077f387
Ayush Gupta <94870982+AyushGupta2114@users.noreply.github.com>
2023-12-31 02:25:48 +0530

    Update file 12/31/2023, 2:25:47 AM

--- 1897-RedistributeCharacterstoMakeAllStringsEqual/1897-RedistributeCharacterstoMakeAllStringsEqual.py
+++ 1897-RedistributeCharacterstoMakeAllStringsEqual/1897-RedistributeCharacterstoMakeAllStringsEqual.py
@@ -2,8 +2,8 @@
         for i,j in B.items():
             if(j%len(words)!=0):
         print(B)
-      ··#·if(len(words)==1):
-········#·····return·True
+        # if(len(words)==1):
+        #     return True
         print(len(words))
     def makeEqual(self, words: List[str]) -> bool:
         a="".join(words)
