5fc725f458d3020fd846ae740ac634a8dcc3b9da
Ayush Gupta <94870982+AyushGupta2114@users.noreply.github.com>
2023-12-31 02:25:31 +0530

    Create file

---
+++ 1897-RedistributeCharacterstoMakeAllStringsEqual/1897-RedistributeCharacterstoMakeAllStringsEqual.py
@@ -0,0 +1,12 @@
+        B=Counter(a)
+        for i,j in B.items():
+            if(j%len(words)!=0):
+        print(B)
+        if(len(words)==1):
+            return True
+        print(len(words))
+                return False
+        return True
+
+
+[
