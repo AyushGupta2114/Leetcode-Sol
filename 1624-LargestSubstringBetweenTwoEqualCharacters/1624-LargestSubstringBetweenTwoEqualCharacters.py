d80f100ba406878b304f2c9a37553696f3560c5e
Ayush Gupta <94870982+AyushGupta2114@users.noreply.github.com>
2024-01-01 02:22:56 +0530

    Update file 1/1/2024, 2:22:55 AM

--- 1624-LargestSubstringBetweenTwoEqualCharacters/1624-LargestSubstringBetweenTwoEqualCharacters.py
+++ 1624-LargestSubstringBetweenTwoEqualCharacters/1624-LargestSubstringBetweenTwoEqualCharacters.py
@@ -3,18 +3,18 @@
             else:
                 # If the character is already in the dictionary, update its last appearance
                 appearances[char]["last"] = i
-                # If the character is not in the dictionary, store its first appearance
                 appearances[char] = {"first": i, "last": i}
-
-# Dictionary to store the first and last appearance of each character
-        appearances = {}
-
-# Variable to store the maximum distance
+                # If the character is not in the dictionary, store its first appearance
+            if char not in appearances:
         max1 = -1
 
         for i, char in enumerate(s):
-            if char not in appearances:
+
+# Variable to store the maximum distance
     def maxLengthBetweenEqualCharacters(self, s: str) -> int:
+
+# Dictionary to store the first and last appearance of each character
+        appearances = {}
 
 class Solution:
 "aa"
