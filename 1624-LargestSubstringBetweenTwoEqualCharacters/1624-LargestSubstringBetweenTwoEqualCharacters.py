99ace430e87d1f52c1546d9048c4c0f3b9423e56
Ayush Gupta <94870982+AyushGupta2114@users.noreply.github.com>
2024-01-01 01:53:39 +0530

    Create file

---
+++ 1624-LargestSubstringBetweenTwoEqualCharacters/1624-LargestSubstringBetweenTwoEqualCharacters.py
@@ -0,0 +1,21 @@
+                max1 = max(max1, appearances[char]["last"] - appearances[char]["first"])
+                # Update the maximum distance if needed
+                appearances[char]["last"] = i
+                # If the character is already in the dictionary, update its last appearance
+            else:
+
+        # Print the maximum distance
+        print("Maximum distance between first and last appearances:", max1)
+        if(max1==-1):
+            return -1
+                # If the character is not in the dictionary, store its first appearance
+                appearances[char] = {"first": i, "last": i}
+            if char not in appearances:
+        for i, char in enumerate(s):
+
+        return max1-1
+        # old method 
+        # max=-1
+        # a=[]
+"
+6
