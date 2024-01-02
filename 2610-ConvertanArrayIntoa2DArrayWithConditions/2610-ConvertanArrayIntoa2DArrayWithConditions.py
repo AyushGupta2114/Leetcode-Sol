932f0cf05f0287e9d3a8bd0b5948ef557a205732
Ayush Gupta <94870982+AyushGupta2114@users.noreply.github.com>
2024-01-03 00:49:58 +0530

    Create file

---
+++ 2610-ConvertanArrayIntoa2DArrayWithConditions/2610-ConvertanArrayIntoa2DArrayWithConditions.py
@@ -0,0 +1,17 @@
+        for num in nums:
+            if num not in count_nums:
+                count_nums[num] = 0
+
+        count_nums = {}
+        ans = []
+class Solution:
+    def findMatrix(self, nums: List[int]) -> List[List[int]]:
+#             count1[j]=b
+#             print(c)
+#         return []
+#                      if item in b:
+#                         c.remove(item)
+#                 for item in a:
+#                     b.append(i)
+#                 if i not in b:
+[
