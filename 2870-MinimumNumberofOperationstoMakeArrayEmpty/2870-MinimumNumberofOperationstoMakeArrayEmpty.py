575eb4925fb4872177803bc2d3744d5b64460434
Ayush Gupta <94870982+AyushGupta2114@users.noreply.github.com>
2024-01-05 03:24:41 +0530

    Update file 1/5/2024, 3:24:41 AM

--- 2870-MinimumNumberofOperationstoMakeArrayEmpty/2870-MinimumNumberofOperationstoMakeArrayEmpty.py
+++ 2870-MinimumNumberofOperationstoMakeArrayEmpty/2870-MinimumNumberofOperationstoMakeArrayEmpty.py
@@ -1,16 +1,16 @@
-        return ans
-                ans+=j//3+1
-                ans+=j//3
-            else:
-        for i,j in a .items():
-            if(j%3==0):
-            return -1
-        if(1 in a.values()):
-        print(a)
-        a=Counter(nums)
-        ans=0
-    def minOperations(self, nums: List[int]) -> int:
-class Solution:
+        #         ans+=j//3+1
+        #         ans+=j//3
+        #     else:
+        # for i,j in a .items():
+        #     if(j%3==0):
+        #     return -1
+        # if(1 in a.values()):
+        # print(a)
+        # return ans
         
+        # a=Counter(nums)
+        # ans=0
+        
+
         
 [2,3,3,2,2,4,2,3,4]
