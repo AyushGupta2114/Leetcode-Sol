e2bcff466860f591e2f79e5bcf3de4e4e61e507c
Ayush Gupta <94870982+AyushGupta2114@users.noreply.github.com>
2024-01-05 03:29:14 +0530

    Update file 1/5/2024, 3:29:13 AM

---  Points/ Points.py
+++  Points/ Points.py
@@ -1,16 +1,16 @@
-        return ans
-                ans+=j//3+1
-                ans+=j//3
-            else:
-        for i,j in a .items():
-            if(j%3==0):
-            return -1
-        if(1 in a.values()):
-        print(a)
-        a=Counter(nums)
-        ans=0
-    def minOperations(self, nums: List[int]) -> int:
 class Solution:
+    def minOperations(self, nums: List[int]) -> int:
+       ·ans=0
+········a=Counter(nums)
+········print(a)
+········if(1·in·a.values()):
+············return·-1
+········for·i,j·in·a·.items():
+············if(j%3==0):
+················ans+=j//3
+············else:
+················ans+=j//3+1
+········return·ans
         
         
 [2,3,3,2,2,4,2,3,4]
