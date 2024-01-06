6463c70a5411ba4103a8e39a1d23591bc8e6d64a
Ayush Gupta <94870982+AyushGupta2114@users.noreply.github.com>
2024-01-07 04:08:56 +0530

    Create file

---
+++ 1103-DistributeCandiestoPeople/1103-DistributeCandiestoPeople.py
@@ -0,0 +1,18 @@
+                i=0
+            if(k<=candies):
+                a[i]=k+a[i]
+                candies-=k
+                k+=1
+            else:
+                a[i]+=candies
+                break
+            i+=1
+        return a
+
+
+            if(i==len(a)):
+        while(candies>0):
+        k=1
+        
+7
+[
