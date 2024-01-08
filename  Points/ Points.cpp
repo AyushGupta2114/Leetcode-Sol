fcd8d3b88d2d86401c180c20c02316a9a9dcd519
Ayush Gupta <94870982+AyushGupta2114@users.noreply.github.com>
2024-01-09 00:16:17 +0530

    Update file 1/9/2024, 12:16:15 AM

---  Points/ Points.cpp
+++  Points/ Points.cpp
@@ -1,16 +1,16 @@
-        {
-            return 1;
-        }
-        cout<<root->val;
-        if(root->val>=low && root->val<=high)
-        {
-            s=s+root->val;
-        }
-        rangeSumBST(root->left,low,high);
-
-        rangeSumBST(root->right,low,high);
-        return s;
-    }
-    
-};
+#include <iostream>
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+    public:
+    int s=0;
 [10,5,15,3,7,null,18]
