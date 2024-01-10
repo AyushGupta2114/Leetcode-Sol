d434c1b9e2e8e3806e97fe9cea1295a3f13b5e6a
Ayush Gupta <94870982+AyushGupta2114@users.noreply.github.com>
2024-01-10 11:17:24 +0530

    Update file 1/10/2024, 11:17:19 AM

--- 872-Leaf-SimilarTrees/872-Leaf-SimilarTrees.cpp
+++ 872-Leaf-SimilarTrees/872-Leaf-SimilarTrees.cpp
@@ -1,16 +1,16 @@
-        vector<int>tree1;
-        vector<int>tree2;
-
-        dfs(root1,tree1);
-        dfs(root2,tree2);
-    if(tree1==tree2)
     {
-    return true;
-    }
-    else
-    {
-        return false;
-    }
-    }
-};
+class Solution {
+public:
+    void dfs(TreeNode* root,vector<int>&v)
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+/**
+ * Definition for a binary tree node.
 [3,5,1,6,2,9,8,null,null,7,4]
