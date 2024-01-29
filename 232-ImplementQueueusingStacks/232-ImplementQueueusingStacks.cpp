f48ec537b00f2f7bb5d4f2306fab8b4e402d1761
Ayush Gupta <94870982+AyushGupta2114@users.noreply.github.com>
2024-01-29 18:57:10 +0530

    Create file

---
+++ 232-ImplementQueueusingStacks/232-ImplementQueueusingStacks.cpp
@@ -0,0 +1,16 @@
+        int temp=st.top();
+        st.pop();
+            return ;
+        }
+            st.push(x);
+        {
+        if(st.size()==0)
+    void push(int x) {
+    
+    }
+        
+    MyQueue() { 
+    stack <int> st;
+public:
+class MyQueue {
+["MyQueue","push","push","peek","pop","empty"]
