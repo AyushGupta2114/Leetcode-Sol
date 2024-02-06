4cc24ba4920dde81f2dc6f54c84e240525e0d5de
Ayush Gupta <94870982+AyushGupta2114@users.noreply.github.com>
2024-02-06 08:59:47 +0530

    Update file 2/6/2024, 8:59:46 AM

---  Points/ Points.py
+++  Points/ Points.py
@@ -1,16 +1,10 @@
-        count1=0
-        count2=0
-        for i in f:
-            if(i in j):
-                count1+=1
-        for i in g:
-            if(i in j):
-        j=['a','e','i','o','u','A','E','I','O','U']
-        g=s[int(k):]
-        print(f)
-    def halvesAreAlike(self, s: str) -> bool:
-        k=len(s)/2
-        print(k)
-        f=s[:int(k)]
 class Solution:
-"book"
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        d=defaultdict(list)
+        for i in strs:
+            d[''.join(sorted(i))].append(i)
+        return d.values()                    
+
+                    
+        
+["eat","tea","tan","ate","nat","bat"]
