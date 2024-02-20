d7e337f5c1a5f8610d1890cc22f9a678f0010c33
Ayush Gupta <94870982+AyushGupta2114@users.noreply.github.com>
2024-02-20 23:38:17 +0530

    Create file

---
+++ 268-MissingNumber/268-MissingNumber.py
@@ -0,0 +1,9 @@
+class Solution:
+    def missingNumber(self, nums: List[int]) -> int:
+        a=0
+        for i in range(0,len(nums)+1):
+            if(i not in nums):
+                a=i
+                break
+        return a
+[3,0,1]
