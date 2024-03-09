116581d8a01c2c7b08e85edd4c0f563f5131233f
Ayush Gupta <94870982+AyushGupta2114@users.noreply.github.com>
2024-03-10 01:59:34 +0530

    Update file 3/10/2024, 1:59:33 AM

--- 2540-MinimumCommonValue/2540-MinimumCommonValue.py
+++ 2540-MinimumCommonValue/2540-MinimumCommonValue.py
@@ -2,16 +2,16 @@
             return False
                 else:
                     right = mid - 1
+        # Check which array is shorter for efficient searching
                 elif arr[mid] < target:
                     left = mid + 1
-        # Check which array is shorter for efficient searching
-        if len(nums1) > len(nums2):
-            nums1, nums2 = nums2, nums1
-        
-        # Perform binary search on the shorter array
-        for num in nums1:
-            if binary_search(nums2, num):
-                return num
-        return -1  # If no common element found
-
+                    return True
+                if arr[mid] == target:
+                mid = left + (right - left) // 2
+            while left <= right:
+            left, right = 0, len(arr) - 1
+        def binary_search(arr, target):
+    def getCommon(self, nums1: List[int], nums2: List[int]) -> int:
+class Solution:
+from typing import List
 [
