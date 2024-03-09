45b96d4b282e3b43320dd8046c4d4f46c9a947bc
Ayush Gupta <94870982+AyushGupta2114@users.noreply.github.com>
2024-03-10 02:19:46 +0530

    Create file

---
+++ 2864-MaximumOddBinaryNumber/2864-MaximumOddBinaryNumber.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    string maximumOddBinaryNumber(string s) {
+        sort(s.rbegin(),s.rend());
+        // cout>>s;
+        if(s[0]=='1'){
+          s.erase(0,1);
+          s +='1';  
+        }
+        return s;
+    }
+};
+"
