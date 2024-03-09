538d4f527d0132def113c0efd9d2e25288573e77
Ayush Gupta <94870982+AyushGupta2114@users.noreply.github.com>
2024-03-10 01:42:06 +0530

    Update file 3/10/2024, 1:42:06 AM

--- 3005-CountElementsWithMaximumFrequency/3005-CountElementsWithMaximumFrequency.py
+++ 3005-CountElementsWithMaximumFrequency/3005-CountElementsWithMaximumFrequency.py
@@ -1,17 +1,15 @@
         for i,j in a.items():
             if j>max:
                 max=j
-        print(a.values())
         for i in a.values():
         sum=0
             if i==max:
                 sum+=i
-        print(max) 
-            print(i)
         return sum
         max=-1
-        print(a)
         a=Counter(nums)
     def maxFrequencyElements(self, nums: List[int]) -> int:
+class Solution:
+        
         
 [
