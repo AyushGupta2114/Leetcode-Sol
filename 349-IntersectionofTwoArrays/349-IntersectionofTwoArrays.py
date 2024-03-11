9a8d1dbb06b49d3467c27800904c394f1646a624
Ayush Gupta <94870982+AyushGupta2114@users.noreply.github.com>
2024-03-11 22:15:53 +0530

    Create file

---
+++ 349-IntersectionofTwoArrays/349-IntersectionofTwoArrays.py
@@ -0,0 +1,13 @@
+#                 mid=left+(right-left)//2
+#             while left<=right:
+#             left,right=0,len(arr)-1
+# Example usage:
+#         def binary_search(arr,target):
+
+        return intersection
+        intersection = list(nums1_set.intersection(nums2_set))
+        nums2_set = set(nums2)
+        nums1_set = set(nums1)
+    def intersection(self, nums1: List[int], nums2: List[int]) -> List[int]:
+class Solution:
+[
