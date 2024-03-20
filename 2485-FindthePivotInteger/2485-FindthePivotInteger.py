2e641847a318084bd42c5b3dffa6c5b1dbe2ca7f
Ayush Gupta <94870982+AyushGupta2114@users.noreply.github.com>
2024-03-21 00:57:06 +0530

    Update file 3/21/2024, 12:57:04 AM

--- 2485-FindthePivotInteger/2485-FindthePivotInteger.py
+++ 2485-FindthePivotInteger/2485-FindthePivotInteger.py
@@ -3,12 +3,11 @@
             }
             if(s1==s2)
             s2=0;
-        };
+        }
         if(pivot==0)
         return -1;
             pivot=i;
         else
-            s1+=i;
         return pivot;
     }
 };
