04c0667adcb3e0fd63e7bda6eaa8af518250cb15
Ayush Gupta <94870982+AyushGupta2114@users.noreply.github.com>
2024-03-21 01:14:56 +0530

    Update file 3/21/2024, 1:14:54 AM

--- 2485-FindthePivotInteger/2485-FindthePivotInteger.cpp
+++ 2485-FindthePivotInteger/2485-FindthePivotInteger.cpp
@@ -1,17 +1,13 @@
-            }
-                if(s2<s1){
-                    continue;
-                }
-            if(s1==s2){
-            pivot=i;
-            break;
-                s2+=j;
-            for(int j=i;j<=n;j++){
-            s1+=i;
-        {
-        for(int i=1;i<=n;i++)
-        int s1=0,s2=0,pivot=0;
-    int pivotInteger(int n) {
-public:
 class Solution {
+public:
+    int pivotInteger(int n) {
+        int sum = n * (n + 1) / 2;
+        double a = sqrt(sum);
+
+        if (a - ceil(a) == 0)
+            return int(a);
+        else
+            return -1;
+    }
+};
 8
