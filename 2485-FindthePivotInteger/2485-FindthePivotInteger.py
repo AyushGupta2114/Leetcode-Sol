8e7056e7a49c279f8fe313eaa29020ed0d9693ce
Ayush Gupta <94870982+AyushGupta2114@users.noreply.github.com>
2024-03-21 00:57:48 +0530

    Update file 3/21/2024, 12:57:46 AM

--- 2485-FindthePivotInteger/2485-FindthePivotInteger.py
+++ 2485-FindthePivotInteger/2485-FindthePivotInteger.py
@@ -1,14 +1,19 @@
-                s2+=j;
-            for(int j=i;j<=n;j++){
-            }
-            if(s1==s2)
-            s2=0;
-        }
-        if(pivot==0)
-        return -1;
-            pivot=i;
-        else
-        return pivot;
-    }
-};
+        #     if(a==b):
+        #         x=False
+        # print(i)
+
+        for i in range(1,n+1):
+            a=int((i*(i+1))/2)
+            b=int(((n-i+1)/2)*(n+i))
+            print(a,b)
+            if a==b:
+                return i
+        return -1
+        #     print(a,b)
+        #     b+=i
+        #     i+=1
+        #     n-=1
+        #     a+=n
+        # while(x):
+        # x=True
 8
