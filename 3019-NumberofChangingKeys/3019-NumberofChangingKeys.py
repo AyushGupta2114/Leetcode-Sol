ba4191ee0ce2eea4ed8625cf060b64e0fc3c853b
Ayush Gupta <94870982+AyushGupta2114@users.noreply.github.com>
2024-03-21 19:07:59 +0530

    Create file

---
+++ 3019-NumberofChangingKeys/3019-NumberofChangingKeys.py
@@ -0,0 +1,12 @@
+class Solution:
+    def countKeyChanges(self, s: str) -> int:
+        c=0
+        s=s.lower()
+            if(s[i]!=s[i+1]):
+                c+=1
+        return c
+        for i in range(0,len(s)-1):
+                # print(s[i],s[i+1])
+        print(s.lower())
+        
+"
