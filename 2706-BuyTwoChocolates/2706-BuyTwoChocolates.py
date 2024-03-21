41f76cf5b2c687419f80226f4407a6165afe990b
Ayush Gupta <94870982+AyushGupta2114@users.noreply.github.com>
2024-03-21 23:33:48 +0530

    Create file

---
+++ 2706-BuyTwoChocolates/2706-BuyTwoChocolates.py
@@ -0,0 +1,13 @@
+        prices.sort()
+        for i in range(0,len(prices)):
+
+            for j in range(0,i):
+
+        return money
+    def buyChoco(self, prices: List[int], money: int) -> int:
+class Solution:
+                if(prices[i]+prices[j]<=money):
+                    return money-(prices[i]+prices[j])
+                    print(prices[i],prices[j])
+        
+[
