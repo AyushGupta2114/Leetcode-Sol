cb8f1ef2a79158480f13a7c11977428ed4bdca31
Ayush Gupta <94870982+AyushGupta2114@users.noreply.github.com>
2024-03-21 14:24:04 +0530

    Update file 3/21/2024, 2:24:04 PM

--- 205-IsomorphicStrings/205-IsomorphicStrings.py
+++ 205-IsomorphicStrings/205-IsomorphicStrings.py
@@ -3,19 +3,19 @@
                 value=a.get(t[i])
         a={}
             return False
-            print(list(x.values()),list(y.values()))
+            # print(list(x.values()),list(y.values()))
         if(list(x.values())!=list(y.values())):
-        y=Counter(t)
-        x=Counter(s)
-    def isIsomorphic(self, s: str, t: str) -> bool:
-class Solution:
                 if (a[s[i]]!=t[i]):
                     return False
                     
             else:
                 a[s[i]]=t[i]
-        print(a)
+        # print(a)
         return True
 
         
+        x=Counter(s)
+        y=Counter(t)
+    def isIsomorphic(self, s: str, t: str) -> bool:
+class Solution:
 "
