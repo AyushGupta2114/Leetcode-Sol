ff799dfa4374aab6e593ec3abc9666b5ecb21b93
Ayush Gupta <94870982+AyushGupta2114@users.noreply.github.com>
2024-03-21 14:45:08 +0530

    Update file 3/21/2024, 2:45:08 PM

--- 2485-FindthePivotInteger/2485-FindthePivotInteger.py
+++ 2485-FindthePivotInteger/2485-FindthePivotInteger.py
@@ -1,16 +1,16 @@
-        # print(i)
-
-        for i in range(1,n+1):
-            a=(i*(i+1))/2
-            b=((n-i+1)/2)*(n+i)
-            # print(a,b)
-            if a==b:
-                return i
-        return -1
-            
-
-
-        
-        #         x=False
+class Solution:
+    def pivotInteger(self, n: int) -> int:
+        # a=0
+        # b=0
+        # i=1
+        # x=True
+        # while(x):
+        #     a+=n
+        #     n-=1
+        #     b+=i
+        #     i+=1
+        #     print(a,b)
         #     if(a==b):
+        #         x=False
+        # print(i)
 8
