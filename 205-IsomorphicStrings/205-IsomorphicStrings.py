0e39a9f25d53aa4ebcea63c956ab2e67a90cf335
Ayush Gupta <94870982+AyushGupta2114@users.noreply.github.com>
2024-03-21 14:23:31 +0530

    Create file

---
+++ 205-IsomorphicStrings/205-IsomorphicStrings.py
@@ -0,0 +1,12 @@
+        a={}
+        for i in range(0,len(s)):
+            if(s[i] in a):
+                value=a.get(t[i])
+            return False
+            print(list(x.values()),list(y.values()))
+        if(list(x.values())!=list(y.values())):
+        x=Counter(s)
+        y=Counter(t)
+    def isIsomorphic(self, s: str, t: str) -> bool:
+class Solution:
+"
