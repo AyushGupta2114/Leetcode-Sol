a19e83d7843f24ae1501ff2e20351410ccd17a71
Ayush Gupta <94870982+AyushGupta2114@users.noreply.github.com>
2024-03-21 13:28:31 +0530

    Create file

---
+++ 206-ReverseLinkedList/206-ReverseLinkedList.py
@@ -0,0 +1,11 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     struct ListNode *next;
+ * };
+ */
+struct ListNode* reverseList(struct ListNode* head) {
+    
+}
+[1,2,3,4,5]
