3d03111e3b3650dedec0f70b4f3a69e1566c36bc
Ayush Gupta <94870982+AyushGupta2114@users.noreply.github.com>
2024-03-23 04:19:40 +0530

    Create file

---
+++ 234-PalindromeLinkedList/234-PalindromeLinkedList.cpp
@@ -0,0 +1,16 @@
+        printf("%d",a);
+            q=q->next;
+        }
+            a++;
+        while(q!=NULL)
+        {
+        int a=0,i=0;
+        struct ListNode *p=head;
+        struct ListNode *q=head;
+        
+    {
+    bool isPalindrome(ListNode* head)
+public:
+class Solution {
+#include <algorithm>
+[1,2,2,1]
