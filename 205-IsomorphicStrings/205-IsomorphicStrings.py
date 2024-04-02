b625e3b0bc54e5ffc837cd2aad006ba486c794b9
Ayush Gupta <94870982+AyushGupta2114@users.noreply.github.com>
2024-04-02 22:55:45 +0530

    Update file 4/2/2024, 10:55:44 PM

--- 205-IsomorphicStrings/205-IsomorphicStrings.py
+++ 205-IsomorphicStrings/205-IsomorphicStrings.py
@@ -1,16 +1,18 @@
+class Solution:
+    def isIsomorphic(self, s: str, t: str) -> bool:
+        # x=Counter(s)
+        # y=Counter(t)
+        # if(list(x.values())!=list(y.values())):
+        #     # print(list(x.values()),list(y.values()))
+        #     return False
+        a={}
         for i in range(0,len(s)):
             if(t[i] in a):
-        a={}
-        #     return False
-        #     # print(list(x.values()),list(y.values()))
                 if(a[t[i]]!=s[i]):
                     return False   
             else:
-                a[t[i]]=s[i]
-        return True
-
-                # print(a)
                 if(s[i] in list(a.values())):
                     return False
-        
-"
+                a[t[i]]=s[i]
+                # print(a)
+"egg"
