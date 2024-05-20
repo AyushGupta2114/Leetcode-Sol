182eeafbfee2332a495f0368e2d14b783587c0fc
Ayush Gupta <94870982+AyushGupta2114@users.noreply.github.com>
2024-05-20 23:10:20 +0530

    Create file

---
+++ 1863-SumofAllSubsetXORTotals/1863-SumofAllSubsetXORTotals.py
@@ -0,0 +1,22 @@
+                xor_result ^= num
+            return xor_result
+
+        total_xor_sum = 0
+        def xor_of_subset(subset):
+            xor_result = 0
+            for num in subset:
+        result = []
+        backtrack(0, [])
+
+                path.pop()
+
+            for i in range(start, len(nums)):
+                path.append(nums[i])
+                backtrack(i + 1, path)
+        def backtrack(start, path):
+            result.append(path[:])
+    def subsetXORSum(self, nums: List[int]) -> int:
+
+class Solution:
+from typing import List
+[
