533dd58de19eaec1a5d925428bcd9bd97ad7360c
Ayush Gupta <94870982+AyushGupta2114@users.noreply.github.com>
2024-05-21 13:26:03 +0530

    Update file 5/21/2024, 1:26:02 PM

---  Points/ Points.py
+++  Points/ Points.py
@@ -1,18 +1,16 @@
-# Definition for a binary tree node.
-# class TreeNode:
-#     def __init__(self, val=0, left=None, right=None):
-#         self.val = val
-#         self.left = left
-#         self.right = right
 class Solution:
-    summ=0
-    def sumOfLeftLeaves(self, root: Optional[TreeNode]) -> int:
-        if(root):
-            if root.left and not root.left.left and not root.left.right:
-                self.summ+=root.left.val
-            self.sumOfLeftLeaves(root.left)
-            self.sumOfLeftLeaves(root.right)
-        return self.summ
+    def subsets(self, nums: List[int]) -> List[List[int]]:
+            result.append(path[:])
+            for i in range(start, len(nums)):
+                path.append(nums[i])
+                backtrack(i + 1, path)
+                path.pop()
 
+        result = []
+        backtrack(0, [])
         
-[3,9,20,null,null,15,7]
+        def backtrack(start, path):
+        print(result)
+        return result
+        
+[
