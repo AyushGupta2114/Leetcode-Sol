a484d72ed309948902979c2d2a533ffa1e040f44
Ayush Gupta <94870982+AyushGupta2114@users.noreply.github.com>
2024-06-05 23:32:42 +0530

    Create file

---
+++ 2956-FindCommonElementsBetweenTwoArrays/2956-FindCommonElementsBetweenTwoArrays.py
@@ -0,0 +1,14 @@
+class Solution:
+    def findIntersectionValues(self, nums1: List[int], nums2: List[int]) -> List[int]:
+        left=0
+        for i in nums1:
+            if i in nums2:
+                left+=1
+        print(left)
+        for i in nums2:
+            if i in nums1:
+                right+=1
+        return [left,right]
+        right=0
+        
+[
