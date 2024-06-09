77e3d234cd5090fc0cf77184637c33dba50842ca
Ayush Gupta <94870982+AyushGupta2114@users.noreply.github.com>
2024-06-10 03:41:53 +0530

    Update file 6/10/2024, 3:41:33 AM

--- 844-BackspaceStringCompare/844-BackspaceStringCompare.py
+++ 844-BackspaceStringCompare/844-BackspaceStringCompare.py
@@ -1,17 +1,5 @@
-        # print(a)
-        for i in t:
-            if(i!='#'):
-                ab.append(i)
-            elif i=='#' and len(ab)!=0:
-        ab=[]
-                a.pop()
-            elif i=='#' and len(a)!=0:
-                a.append(i)
-                ab.pop()
-        print(ab)
-        for i in s:
-            if i!='#':
-    def backspaceCompare(self, s: str, t: str) -> bool:
-        a=[]
 class Solution:
+    def uncommonFromSentences(self, s1: str, s2: str) -> List[str]:
+        
+        
 "
