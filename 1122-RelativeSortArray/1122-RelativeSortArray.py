f5483db97b7e05e5685f5ecdc2ad1eb8b9c178dd
Ayush Gupta <94870982+AyushGupta2114@users.noreply.github.com>
2024-06-12 02:36:51 +0530

    Update file 6/12/2024, 2:36:50 AM

--- 1122-RelativeSortArray/1122-RelativeSortArray.py
+++ 1122-RelativeSortArray/1122-RelativeSortArray.py
@@ -1,17 +1,17 @@
-        a=[]
-        b=[]
-        for i in arr2:
             if i not in arr1:
-                a.append(i)
                 arr1.remove(i)
-        print(a)
+        # print(a)
         for i in arr2:
             while(i in arr1):
                 b.append(i)
                 arr1.remove(i)
-        print(b)
-        return b+arr1
-        print(arr1)
+        # print(b)
+        # print(arr1)
         arr1.sort()
+        for i in arr2:
+        b=[]
+        return b+arr1
 
+    def relativeSortArray(self, arr1: List[int], arr2: List[int]) -> List[int]:
+class Solution:
 [
