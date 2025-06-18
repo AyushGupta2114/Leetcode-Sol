5ad734fec0d222e7b4127c0847a3d43a8a4479fa
Ayush Gupta <94870982+AyushGupta2114@users.noreply.github.com>
2025-06-18 22:39:25 +0530

    Update file 6/18/2025, 10:39:25 PM

--- 2966-DivideArrayIntoArraysWithMaxDifference/2966-DivideArrayIntoArraysWithMaxDifference.py
+++ 2966-DivideArrayIntoArraysWithMaxDifference/2966-DivideArrayIntoArraysWithMaxDifference.py
@@ -1,1 +1,11 @@
-        print(nums)
+        a=[]
+        for i in range(0,len(nums),3):
+            if(nums[i+2]-nums[i]>k) or (nums[i+1]-nums[i]>k):
+                return []
+            else:
+                a.append(nums[i:i+3])
+        nums.sort()
+    def divideArray(self, nums: List[int], k: int) -> List[List[int]]:
+class Solution:
+
+        return a
