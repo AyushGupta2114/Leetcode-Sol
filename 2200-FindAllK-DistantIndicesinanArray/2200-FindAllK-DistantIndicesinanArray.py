71da27bbce798e6856074d4599672e35a7d14a0f
Ayush Gupta <94870982+AyushGupta2114@users.noreply.github.com>
2025-06-24 22:32:13 +0530

    Create file

---
+++ 2200-FindAllK-DistantIndicesinanArray/2200-FindAllK-DistantIndicesinanArray.py
@@ -0,0 +1,10 @@
+    def findKDistantIndices(self, nums: List[int], key: int, k: int) -> List[int]:
+        pos=[]
+        for i in range(0,len(nums)):
+            for j in range(0,len(nums)):
+                if((nums[j]==key) and (abs(i-j))<=k):
+                    print(nums[j],abs(i-j))
+                    pos.append(i)
+class Solution:
+                
+                    break
